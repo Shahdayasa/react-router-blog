7c36a66578b9a4b08d8f0052b09e2b1033f94732
shahd <shahdayasa46@gmail.com>
2026-09-02 11:29:22 +0300

    Add base path to vite config for GitHub Pages assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+// vite.config.ts
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/react-router-blog/",
   plugins: [
     tailwindcss(),
     reactRouter(),
