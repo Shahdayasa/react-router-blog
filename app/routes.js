f35dd79fd884111d4e803f410788f0fe6dddb1e8
shahd <shahdayasa46@gmail.com>
2026-09-07 11:46:33 +0300

    fix duplicate route id

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 import { index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.jsx"),
+  index("routes/blog.jsx", { id: "home" }),
   route("blog", "routes/blog.jsx"),
   route("signup", "routes/signup.jsx"),
   route(":year/:month/:day/:slug", "routes/article.jsx"),
