3aa319a5535bd47af0c5425400be02d15ef0b743
shahd <shahdayasa46@gmail.com>
2026-09-07 12:36:54 +0300

    enable prerendering to fix CLS

--- react-router.config.js
+++ react-router.config.js
@@ -1,4 +1,16 @@
+import { articles } from "./app/data/articles.js";
+
 export default {
   ssr: false,
   basename: "/react-router-blog/",
+
+  async prerender() {
+    return [
+      "/",
+      ...articles.map(
+        (article) =>
+          `/${article.year}/${article.month}/${article.day}/${article.slug}`
+      ),
+    ];
+  },
 };
